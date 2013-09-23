38454f10d3f15b74c5b12355d8d373f73e60b243
lanqu <lanqu@yandex-team.ru>
2013-09-24 02:23:32 +0400

    init

--- gen/com/kotoblog/mybenua/BuildConfig.java
+++ gen/com/kotoblog/mybenua/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.benua;
+package com.kotoblog.mybenua;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
